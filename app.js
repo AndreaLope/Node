57ea51c198d10c963ee883add5a735dc72c35d19
AndreaLope <54321503+AndreaLope@users.noreply.github.com>
2019-09-17 11:10:57 -0500

    valores

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
 const fs= require("fs");
 const express=require("express")
-//const app=express();
-//const PORT=9000;
+const app=express();
+const PORT=8080;
 //fs.readFileSync("document.json",
 //	(data)=>{
 //		if(err) throw err;
 //		console.log("data ", JSON.parse(data).length);
 //	}
 //);
-//const file=(err,data) => {
-//	if(err) throw err;
-//	console.log("uno")
-//	const dataP= JSON.parse(data);
-//	console.log("data ", dataP.length)
-//};
-//console.log("dos")
-//fs.readFile("document.json", file);
-//console.log("tres")
+const file=(err,data) => {
+	if(err) throw err;
+console.log("uno")
+	const dataP= JSON.parse(data);
+	console.log("data ", dataP.length)
+};
+console.log("dos")
+fs.readFile("document.json", file);
+console.log("tres")
 //let a=1+2;
 //console.log ("a=", a);
-//app.get("/",(req,res)=>{
-//	console.log("Got GET /");
-//	res.send("HOLA JHON")
-//});
-//app.listen(PORT, ()=>
-//{
-//	console.log(`my app is running at http://localhosto:${PORT}`);
-//});
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/",(req,res)=>{
+	console.log("Got GET /");
+	res.send("HOLA JHON 2")
 });
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, ()=>
+{
+	console.log(`my app is running at http://localhosto:${PORT}`);
 });
