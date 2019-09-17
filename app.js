f14ee4d9f0088a9db500bbd124a538c4f9570ac5
AndreaLope <ac.lopez@uniandes.edu.co>
2019-09-16 22:54:06 -0500

    app.js

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+const fs= require("fs");
+//fs.readFileSync("document.json",
+//	(data)=>{
+//		if(err) throw err;
+//		console.log("data ", JSON.parse(data).length);
+//	}
+//);
+const file=(err,data) => {
+	if(err) throw err;
+	console.log("uno")
+	const dataP= JSON.parse(data);
+	console.log("data ", dataP.length)
+};
+console.log("dos")
+fs.readFile("document.json", file);
+console.log("tres")
 let a=1+2;
 console.log ("a=", a);
